ba162dbf52b7c4de4af3cd192837e6ac0fd982fa
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-03-31 20:09:31 +0200

    feat(server/framework): add qb-core and more formats

--- server/framework.lua
+++ server/framework.lua
@@ -4,10 +4,40 @@ if Core.resource == 'ox_core' then
 	function playerGroup(source, group)
 		if not group then return true end
 
-		for k, v in pairs(group) do
-			if IsPlayerAceAllowed(source, ('group.%s:%s'):format(k, v)) then
-				return true
+		if type(group) == 'table' then
+			for k, v in pairs(group) do
+				if type(k) == 'string' then
+					if IsPlayerAceAllowed(source, ('group.%s:%s'):format(k, v)) then
+						return true
+					end
+				else
+					if IsPlayerAceAllowed(source, ('group.%s'):format(v)) then
+						return true
+					end
+				end
 			end
+		else
+			return IsPlayerAceAllowed(source, ('group.%s'):format(group))
+		end
+	end
+elseif Core.resource == 'qb-core' then
+	function playerGroup(source, group)
+		if not group then return true end
+
+		if type(group) == 'table' then
+			for k, v in pairs(group) do
+				if type(k) == 'string' then
+					if IsPlayerAceAllowed(source, ('qbcore.%s:%s'):format(k, v)) then
+						return true
+					end
+				else
+					if IsPlayerAceAllowed(source, ('qbcore.%s'):format(v)) then
+						return true
+					end
+				end
+			end
+		else
+			return IsPlayerAceAllowed(source, ('qbcore.%s'):format(group))
 		end
 	end
 end
