164ce93e58315ab55b694e1bf51f28654e96632e
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-04-01 10:03:43 +0200

    feat(server/framework): add qb-core and support more group formats
    Support string and arrays

--- server/framework.lua
+++ server/framework.lua
@@ -4,10 +4,36 @@ if Core.resource == 'ox_core' then
 	function playerGroup(source, group)
 		if not group then return true end
 
-		for k, v in pairs(group) do
-			if IsPlayerAceAllowed(source, ('group.%s:%s'):format(k, v)) then
+		if type(group) == 'table' then
+			for k, v in pairs(group) do
+				if type(k) == 'string' then
+					if IsPlayerAceAllowed(source, ('group.%s:%s'):format(k, v)) then
+						return true
+					end
+				else
+					if IsPlayerAceAllowed(source, ('group.%s'):format(v)) then
+						return true
+					end
+				end
+			end
+		else
+			return IsPlayerAceAllowed(source, ('group.%s'):format(group))
+		end
+	end
+elseif Core.resource == 'qb-core' then
+	function playerGroup(source, job)
+		if not job then return true end
+
+		local Player = Core.Functions.GetPlayer(source)
+		if not Player then return false end
+
+		if type(job) == 'table' then
+			local rank = job[Player.PlayerData.job.name]
+			if rank and Player.PlayerData.job.grade.level >= rank then
 				return true
 			end
+		else
+			return Player.PlayerData.job.name == job
 		end
 	end
 end
