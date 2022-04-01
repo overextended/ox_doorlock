ce3b70a20b125154f5da69542393e072ecad0304
Linden <65407488+thelindat@users.noreply.github.com>
2022-04-01 07:58:58 +0000

    Merge branch 'main' of https://github.com/overextended/ox_doorlock

--- server/main.lua
+++ server/main.lua
@@ -1,101 +1,49 @@
 local doors = {}
 
--- sample data
+local function createDoor(id, door)
+	local double = door.doors
+
+	if double then
+		for i = 1, 2 do
+			double[i].hash = joaat(('ox_door_%s_%s'):format(id, i))
+
+			local coords = double[i].coords
+			double[i].coords = vector3(coords.x, coords.y, coords.z)
+		end
+
+		door.coords = double[1].coords - ((double[1].coords - double[2].coords) / 2)
+	else
+		door.hash = joaat(('ox_door_%s'):format(id))
+		door.coords = vector3(door.coords.x, door.coords.y, door.coords.z)
+	end
+
+	if not door.state then
+		door.state = 1
+	end
+
+	door.id = id
+	doors[id] = door
+end
+
 do
-	local data = {
-		-- armoury hallway
-		{
-			model = 1557126584,
-			coords = vector3(450.1042, -985.7384, 30.83931),
-			heading = 90,
-			maxDistance = 2,
-			groups = {
-				police = 1,
-			}
-		},
-		-- captain's office
-		{
-			model = -1320876379,
-			coords = vector3(446.5728, -980.0106, 30.83931),
-			heading = 180,
-			maxDistance = 2,
-			groups = {
-				police = 1,
-			}
-		},
-		-- entrance
-		{
-			doors = {
-				{
-					model = -1215222675,
-					heading = 270,
-					coords = vector3(434.7479, -980.6184, 30.83927),
-				},
-				{
-					model = 320433149,
-					heading = 270,
-					coords = vector3(434.7479, -983.2151, 30.83927),
-				}
-			},
-			maxDistance = 3,
-			state = 0,
-			groups = {
-				police = 1,
-			}
-		},
-		-- garage
-		{
-			model = -190780785,
-			coords = vector3(431.4056, -1001.169, 26.71261),
-			heading = 0,
-			maxDistance = 5,
-			groups = {
-				police = 1,
-			},
-			auto = true,
-		},
-		-- back gate
-		{
-			model = -1603817716,
-			coords = vector3(488.8948, -1017.21, 27.14863),
-			heading = 90,
-			maxDistance = 5,
-			groups = {
-				police = 1,
-			},
-			auto = true,
-		},
-	}
+	local data = json.decode(LoadResourceFile('ox_doorlock', 'server/doors.json'))
 
-	for i = 1, #data do
-		local door = setmetatable(data[i], CDoor)
-		local double = door.doors
-
-		if double then
-			double[1].hash = joaat(('ox_door_%s_1'):format(i))
-			double[2].hash = joaat(('ox_door_%s_2'):format(i))
-			door.coords = double[1].coords - ((double[1].coords - double[2].coords) / 2)
+	if data then
+		for i = 1, #data do
+			createDoor(i, data[i])
 		end
-
-		if not door.state then
-			door.state = 1
-		end
-
-		door.hash = joaat(('ox_door_%s'):format(i))
-		doors[door.hash] = door
 	end
 end
 
-RegisterNetEvent('ox_doorlock:setState', function(hash, state)
-	local door = doors[hash]
+RegisterNetEvent('ox_doorlock:setState', function(id, state)
+	local door = doors[id]
 
 	if door and playerGroup(source, door.groups) then
 		door.state = state
-		TriggerClientEvent('ox_doorlock:setState', -1, hash, state)
+		TriggerClientEvent('ox_doorlock:setState', -1, id, state)
 	end
 end)
 
 RegisterNetEvent('ox_doorlock:getDoors', function()
 	TriggerClientEvent('ox_doorlock:setDoors', source, doors)
 end)
-
