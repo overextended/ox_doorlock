fd5cb7f4416222c7498864f75e49c06d10072bdb
Luke <sabolukas03@gmail.com>
2022-04-03 12:05:37 +0200

    feat(nui): Checkbox tooltips

--- web/src/components/Checkboxes.tsx
+++ web/src/components/Checkboxes.tsx
@@ -1,16 +1,53 @@
-import { Box, FormControlLabel, Checkbox } from '@mui/material';
+import { Box, FormControlLabel, Checkbox, Tooltip } from '@mui/material';
 
 const Checkboxes: React.FC = () => {
   return (
     <>
       <Box display="flex">
         <Box display="flex" justifyContent="space-evenly" flexDirection="column">
-          <FormControlLabel label="Double door" control={<Checkbox />} sx={{ margin: 0 }} />
-          <FormControlLabel label="Automatic" control={<Checkbox />} sx={{ margin: 0 }} />
+          <FormControlLabel
+            label="Double door"
+            control={
+              <Tooltip title="Door is a double door" disableInteractive enterDelay={500}>
+                <Checkbox />
+              </Tooltip>
+            }
+            sx={{ margin: 0 }}
+          />
+
+          <FormControlLabel
+            label="Automatic"
+            control={
+              <Tooltip
+                title="Door automatically moves (garage, sliding, etc...)"
+                disableInteractive
+                enterDelay={500}
+              >
+                <Checkbox />
+              </Tooltip>
+            }
+            sx={{ margin: 0 }}
+          />
         </Box>
         <Box display="flex" justifyContent="space-evenly" flexDirection="column">
-          <FormControlLabel label="Locked" control={<Checkbox />} sx={{ margin: 0 }} />
-          <FormControlLabel label="Lockpick" control={<Checkbox />} sx={{ margin: 0 }} />
+          <FormControlLabel
+            label="Locked"
+            control={
+              <Tooltip title="Door is locked by default" disableInteractive enterDelay={500}>
+                <Checkbox />
+              </Tooltip>
+            }
+            sx={{ margin: 0 }}
+          />
+          <FormControlLabel
+            label="Lockpick"
+            control={
+              <Tooltip title="Door can be lockpicked" disableInteractive enterDelay={500}>
+                <Checkbox />
+              </Tooltip>
+            }
+            sx={{ margin: 0 }}
+          />
         </Box>
       </Box>
     </>
