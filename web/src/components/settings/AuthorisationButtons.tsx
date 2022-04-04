1a20324bebb30f12192e30c09e846bfeaa76445e
Luke <sabolukas03@gmail.com>
2022-04-04 22:18:48 +0200

    fix(nui): Use programmatic navigation
    Using href would cause a page refresh and was slow overall

--- web/src/components/settings/AuthorisationButtons.tsx
+++ web/src/components/settings/AuthorisationButtons.tsx
@@ -1,14 +1,26 @@
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const AuthorisationButtons: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      {/* Todo: Use react-router components instead of href */}
-      <Button fullWidth style={{ marginBottom: '0.7rem' }} variant="outlined" href="job_auth">
+      <Button
+        fullWidth
+        style={{ marginBottom: '0.7rem' }}
+        variant="outlined"
+        onClick={() => navigate('/job_auth')}
+      >
         Job Authorisation
       </Button>
 
-      <Button fullWidth style={{ marginBottom: '0.7rem' }} variant="outlined" href="item_auth">
+      <Button
+        fullWidth
+        style={{ marginBottom: '0.7rem' }}
+        variant="outlined"
+        onClick={() => navigate('/item_auth')}
+      >
         Item Authorisation
       </Button>
     </>
