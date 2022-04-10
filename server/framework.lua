55f885bacebf008e673ce7e3cbe2bc6c54b5d678
Linden <65407488+thelindat@users.noreply.github.com>
2022-04-10 23:08:45 +0000

    feat(framework): support qbcore gang and esx job

--- server/framework.lua
+++ server/framework.lua
@@ -21,19 +21,40 @@ if Core.resource == 'ox_core' then
 		end
 	end
 elseif Core.resource == 'qb-core' then
-	function playerGroup(source, job)
-		if not job then return true end
+	function playerGroup(source, group)
+		if not group then return true end
 
-		local Player = Core.Functions.GetPlayer(source)
-		if not Player then return false end
+		local player = Core.Functions.GetPlayer(source)
+		if not player then return false end
 
-		if type(job) == 'table' then
-			local rank = job[Player.PlayerData.job.name]
-			if rank and Player.PlayerData.job.grade.level >= rank then
+		if type(group) == 'table' then
+			local jobGrade = group[player.PlayerData.job.name]
+			if jobGrade and player.PlayerData.job.grade.level >= jobGrade then
+				return true
+			end
+
+			local gangGrade = group[player.PlayerData.gang.name]
+			if gangGrade and player.PlayerData.gang.grade.level >= gangGrade then
 				return true
 			end
 		else
-			return Player.PlayerData.job.name == job
+			return player.PlayerData.job.name == group or player.PlayerData.gang.name == group
+		end
+	end
+elseif Core.resource == 'es_extended' then
+	function playerGroup(source, group)
+		if not group then return true end
+
+		local player = Core.GetPlayerFromId(source)
+		if not player then return false end
+
+		if type(group) == 'table' then
+			local jobGrade = group[player.PlayerData.job.name]
+			if jobGrade and player.PlayerData.job.grade.level >= jobGrade then
+				return true
+			end
+		else
+			return player.PlayerData.job.name == group
 		end
 	end
 end
