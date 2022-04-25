c1dc82bb7a579f82d23abdc0ce517f32de438aac
Linden <65407488+thelindat@users.noreply.github.com>
2022-04-25 13:37:59 +1000

    feat(client/utils): lockpicking

--- client/utils.lua
+++ client/utils.lua
@@ -1,27 +1,81 @@
 local Entity = Entity
 
+local function getDoorFromEntity(entity)
+	local state = Entity(entity).state
+	local door = doors[state.doorId]
+
+	if not door then
+		state.doorId = nil
+	end
+
+	return door
+end
+
 local function entityIsNotDoor(entity)
-	local state = Entity(entity).state
-	local doorId = state.doorId
-
-	if doorId and not doors[doorId] then
-		state.doorId = nil
-	end
-
-	return not doorId
+	return not getDoorFromEntity(entity)
 end
 
-local function entityIsDoor(entity)
-	local state = Entity(entity).state
-	local doorId = state.doorId
+local pickingLock
 
-	if doorId and not doors[doorId] then
-		state.doorId = nil
-		return false
+local function canPickLock(entity)
+	return not pickingLock and getDoorFromEntity(entity)?.lockpick
+end
+
+local function pickLock(entity)
+	pickingLock = true
+	local door = getDoorFromEntity(entity)
+	local success
+	TaskTurnPedToFaceCoord(cache.ped, door.coords.x, door.coords.y, door.coords.z, 4000)
+	Wait(500)
+
+	CreateThread(function()
+		success = not cache.vehicle and lib.progressCircle({
+			duration = 4000,
+			canCancel = true,
+			disable = {
+				move = true,
+				combat = true,
+			},
+			anim = {
+				dict = 'mp_common_heist',
+				clip = 'pick_door',
+			}
+		})
+	end)
+
+	while success == nil do
+		Wait(50)
+		if math.random(1, 500) == 1 then
+			TriggerServerEvent('ox_doorlock:breakLockpick')
+			lib.cancelProgress()
+			lib.notify({ type = 'error', description = 'Your lockpick broke' })
+		end
 	end
 
-	return doorId
+	if math.random(1, 100) == 1 then
+		TriggerServerEvent('ox_doorlock:breakLockpick')
+		lib.notify({ type = 'error', description = 'Your lockpick broke' })
+	end
+
+	if success then
+		TriggerServerEvent('ox_doorlock:setState', door.id, door.state == 1 and 0 or 1, true)
+	end
+
+	pickingLock = false
 end
+
+exports.qtarget:Object({
+	options = {
+		{
+			label = 'Pick lock',
+			icon = 'fas fa-user-lock',
+			action = pickLock,
+			canInteract = canPickLock,
+			item = 'lockpick',
+			distance = 1
+		}
+	}
+})
 
 local tempData = {}
 
@@ -63,7 +117,7 @@ local function parseTempData()
 		auto = tempData.checkboxes.automatic or nil,
 		state = tempData.checkboxes.locked and 1 or 0,
 		lockpick = tempData.checkboxes.lockpick or nil,
-		double = tempData.checkboxes.double and {} or nil,
+		doors = tempData.checkboxes.double and true or nil,
 		groups = {},
 		items = {},
 	}
@@ -127,7 +181,7 @@ local function newDoorlock()
 		}
 	})
 
-	if data.double then
+	if data.doors then
 		repeat Wait(50) until tempData[2]
 		data.doors = tempData
 	else
@@ -156,7 +210,7 @@ local function parseDoorData(door)
 			automatic = door.auto,
 			locked = door.state == 1,
 			lockpick = door.lockpick,
-			double = door.double and true,
+			double = door.doors and true,
 		}
 	}
 
@@ -189,11 +243,15 @@ local function editDoorlock(entity)
 		repeat Wait(50) until next(tempData)
 
 		local data = parseTempData()
-		data.coords = door.coords
-		data.heading = door.heading
-		data.model = door.model
-		data.hash = door.hash
 		data.id = door.id
+		data.coords = door.coords
+		data.doors = door.doors
+
+		if not data.doors then
+			data.heading = door.heading
+			data.model = door.model
+			data.hash = door.hash
+		end
 
 		SetNuiFocus(false, false)
 		table.wipe(tempData)
@@ -203,7 +261,7 @@ end
 
 local function removeTarget(res)
 	if not res or res == 'ox_doorlock' then
-		exports.qtarget:RemoveObject({'Remove doorlock', 'Edit doorlock', 'Add doorlock'})
+		exports.qtarget:RemoveObject({'Remove doorlock', 'Edit doorlock', 'Add doorlock', 'Pick lock'})
 	end
 end
 
@@ -220,13 +278,13 @@ RegisterCommand('doorlock', function(_, args)
 						label = 'Edit doorlock',
 						icon = 'fas fa-file-pen',
 						action = editDoorlock,
-						canInteract = entityIsDoor
+						canInteract = getDoorFromEntity
 					},
 					{
 						label = 'Remove doorlock',
 						icon = 'fas fa-file-circle-minus',
 						action = removeDoorlock,
-						canInteract = entityIsDoor
+						canInteract = getDoorFromEntity
 					},
 				}
 			})
