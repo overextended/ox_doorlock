8ced79b0dd07f4e4b4c0fe90e912fd18ea99e5dc
Linden <65407488+thelindat@users.noreply.github.com>
2022-05-30 15:42:36 +1000

    refactor(server/framework): utilise lib.getPlayer
    Currently only the hasGroup method exists.
    Works for ox, qb, and esx.

--- server/framework.lua
+++ server/framework.lua
@@ -1,26 +1,9 @@
 local Core = lib.getCore()
 local ox_inventory = exports.ox_inventory
 
-
-local function getPlayer(source)
-	return Player(source)
-end
-
-local function hasGroup(source, groups)
-	if not groups then return end
-
-	for k, v in pairs(groups) do
-		if IsPlayerAceAllowed(source, ('group.%s:%s'):format(k, v)) then
-			return true
-		end
-	end
-
-	return false
-end
-
 local function hasItem(player, items)
 	if items[2] then
-		for name, count in pairs(ox_inventory:Search(player.source, 'count', items)) do
+		for name, count in pairs(ox_inventory:Search(player, 'count', items)) do
 			if count > 0 then
 				return name
 			end
@@ -29,15 +12,15 @@ local function hasItem(player, items)
 		end
 	end
 
-	return ox_inventory:GetItem(player.source, items[1], false, true) > 0 and items[1]
+	return ox_inventory:GetItem(player, items[1], false, true) > 0 and items[1]
 end
 
 local function removeItem(player, item)
-	ox_inventory:RemoveItem(player.source, item, 1)
+	ox_inventory:RemoveItem(player, item, 1)
 end
 
 function isAuthorised(source, door, lockpick, passcode)
-	local player, authorised = getPlayer(source)
+	local player, authorised = lib.getPlayer(source)
 	if not player then return end
 
 	if lockpick and door.lockpick then
@@ -49,7 +32,7 @@ function isAuthorised(source, door, lockpick, passcode)
 	end
 
 	if door.groups then
-		authorised = hasGroup(player, door.groups)
+		authorised = player.hasGroup(door.groups)
 	end
 
 	if not authorised and door.items then
@@ -62,22 +45,6 @@ end
 if Core.resource == 'qb-core' then
 	function getPlayer(source)
 		return Core.Functions.GetPlayer(source)
-	end
-
-	function hasGroup(player, groups)
-		local jobGrade = groups[player.PlayerData.job.name]
-
-		if jobGrade and player.PlayerData.job.grade.level >= jobGrade then
-			return true
-		end
-
-		local gangGrade = groups[player.PlayerData.gang.name]
-
-		if gangGrade and player.PlayerData.gang.grade.level >= gangGrade then
-			return true
-		end
-
-		return false
 	end
 
 	function hasItem(player, items)
@@ -98,16 +65,6 @@ if Core.resource == 'qb-core' then
 elseif Core.resource == 'es_extended' then
 	function getPlayer(source)
 		return Core.GetPlayerFromId(source)
-	end
-
-	function hasGroup(player, groups)
-		local jobGrade = groups[player.job.name]
-
-		if jobGrade and player.job.grade >= jobGrade then
-			return true
-		end
-
-		return false
 	end
 
 	if not Core.GetConfig().OxInventory then
