b5147d7a105f14e8e8f5e27637cc5ef5fc7f7c78
Luke <sabolukas03@gmail.com>
2022-08-07 18:47:33 +0200

    refactor(web): improve general settings layout and design

--- web/src/layouts/settings/views/general/general.tsx
+++ web/src/layouts/settings/views/general/general.tsx
@@ -1,15 +1,48 @@
-import { Box, Stack, TextInput, NumberInput } from '@mantine/core';
+import { Box, SimpleGrid, TextInput, NumberInput, Grid, Text, Switch } from '@mantine/core';
 
 const General: React.FC = () => {
   return (
     <Box>
-      <Stack justify="center" align="center" sx={{ width: '100%' }}>
-        <TextInput placeholder="Door name" />
-        <TextInput placeholder="Passcode" />
-        <NumberInput placeholder="Autolock interval" />
-        <NumberInput placeholder="Interact distance" />
-        <NumberInput placeholder="Door rate" />
-      </Stack>
+      <Grid columns={2}>
+        <Grid.Col span={1}>
+          <Box>
+            <Text>Door name</Text>
+            <TextInput />
+          </Box>
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <Box>
+            <Text>Passcode</Text>
+            <TextInput />
+          </Box>
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <Box>
+            <Text>Autolock Interval</Text>
+            <NumberInput />
+          </Box>
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <Box>
+            <Text>Interact Distance</Text>
+            <NumberInput />
+          </Box>
+        </Grid.Col>
+        <Grid.Col span={2}>
+          <Box>
+            <Text>Door Rate</Text>
+            <NumberInput />
+          </Box>
+        </Grid.Col>
+      </Grid>
+
+      <SimpleGrid cols={2} pt={16}>
+        <Switch label="Locked" />
+        <Switch label="Double" />
+        <Switch label="Automatic" />
+        <Switch label="Lockpick" />
+        <Switch label="Hide UI" />
+      </SimpleGrid>
     </Box>
   );
 };
