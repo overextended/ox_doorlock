302ccd49a717b41970a3f7ba1a758c8a8b6a7bc9
Luke <sabolukas03@gmail.com>
2022-08-09 16:50:26 +0200

    feat(web): groups state

---
+++ web/src/layouts/settings/views/groups/components/fields.tsx
@@ -0,0 +1,37 @@
+import { Group, Input, NumberInput } from '@mantine/core';
+import { useStore, useSetters } from '../../../../../store';
+
+const Fields: React.FC = () => {
+  const groupFields = useStore((state) => state.groupFields);
+  const setGroupFields = useSetters((setter) => setter.setGroupFields);
+
+  const handleChange = (value: string | number | undefined, index: number, property: 'name' | 'grade') => {
+    // i hate this
+    setGroupFields((prevState) => {
+      return prevState.map((item, indx) => (index === indx ? { ...item, [property]: value } : item));
+    });
+  };
+
+  return (
+    <>
+      {groupFields.map((field, index) => (
+        <Group p={8} position="center" key={`group-${index}`} noWrap>
+          <Input
+            sx={{ width: '100%' }}
+            placeholder="Group"
+            value={field.name as string}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e.target.value, index, 'name')}
+          />
+          <NumberInput
+            sx={{ width: '100%' }}
+            placeholder="Grade"
+            value={field.grade as number}
+            onChange={(e) => handleChange(e, index, 'grade')}
+          />
+        </Group>
+      ))}
+    </>
+  );
+};
+
+export default Fields;
--- web/src/layouts/settings/views/groups/index.tsx
+++ web/src/layouts/settings/views/groups/index.tsx
@@ -1,18 +1,33 @@
 import { Box, Input, Stack, Button, Group, NumberInput, Tooltip } from '@mantine/core';
+import { useEffect } from 'react';
 import { TbPlus } from 'react-icons/tb';
+import { useSetters } from '../../../../store';
+import Fields from './components/fields';
 
 const Groups: React.FC = () => {
+  const setGroupFields = useSetters((setter) => setter.setGroupFields);
+
+  // Remove empty fields on unmount
+  useEffect(() => {
+    return () => {
+      setGroupFields((prevState) =>
+        prevState.filter((item, index) => item.name !== '' || item.grade !== null || index === 0),
+      );
+    };
+  }, []);
+
   return (
     <Stack justify="space-between" align="center" sx={{ height: '100%' }}>
-      <Box sx={{ width: '100%' }}>
-        <Group p={8} position="center">
-          <Input placeholder="Group" sx={{ width: '40%' }} />
-          <NumberInput placeholder="Grade" sx={{ width: '40%' }} />
-        </Group>
+      <Box sx={{ width: '100%', overflowY: 'auto', maxHeight: 410 }}>
+        <Fields />
       </Box>
 
       <Tooltip label="Create a new row" withArrow arrowSize={10}>
-        <Button fullWidth variant="light">
+        <Button
+          fullWidth
+          variant="light"
+          onClick={() => setGroupFields((prevState) => [...prevState, { name: '', grade: null }])}
+        >
           <TbPlus size={24} />
         </Button>
       </Tooltip>
