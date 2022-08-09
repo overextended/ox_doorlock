aa086fed6844c316d3a14759c32466bea32cdd33
Luke <sabolukas03@gmail.com>
2022-08-09 13:05:01 +0200

    style(web): increase switch icon size and align to center

--- web/src/layouts/settings/views/general/components/TooltipSwitch.tsx
+++ web/src/layouts/settings/views/general/components/TooltipSwitch.tsx
@@ -8,11 +8,11 @@ interface Props {
 
 const TooltipSwitch: React.FC<Props> = ({ infoCircle, label }) => {
   return (
-    <Group spacing={8}>
+    <Group spacing={8} align="center">
       <Switch label={label} />
       <Tooltip label={infoCircle} withArrow arrowSize={10} multiline width={200}>
-        <Box>
-          <BsQuestionCircle size={16} />
+        <Box sx={{ display: 'flex' }}>
+          <BsQuestionCircle size={18} />
         </Box>
       </Tooltip>
     </Group>
