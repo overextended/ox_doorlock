57c888ec2ab78cd542d210d917e9a5231d5e2bb8
Luke <sabolukas03@gmail.com>
2022-08-10 14:27:32 +0200

    feat(web): door settings submitting

--- web/src/layouts/settings/views/general/index.tsx
+++ web/src/layouts/settings/views/general/index.tsx
@@ -1,9 +1,35 @@
 import { Box, Center, Button, Stack } from '@mantine/core';
-import { TbCheck } from 'react-icons/tb';
+import { fetchNui } from '../../../../utils/fetchNui';
+import { useStore } from '../../../../store';
+import { useVisibility } from '../../../../store/visibility';
 import Inputs from './components/Inputs';
 import Switches from './components/Switches';
 
 const General: React.FC = () => {
+  const setVisible = useVisibility((state) => state.setVisible);
+
+  const handleSubmit = () => {
+    const state = useStore.getState();
+    const data = { ...state };
+    if (data.doorName === '') data.doorName = null;
+    if (data.passcode === '') data.passcode = null;
+    data.autolockInterval = data.autolockInterval || null;
+    data.interactDistance = data.interactDistance || null;
+    data.doorRate = data.doorRate || null;
+    for (let i = 0; i < data.itemFields.length; i++) {
+      const itemField = data.itemFields[i];
+      if (itemField.name === '') itemField.name = null;
+      if (itemField.metadata === '') itemField.metadata = null;
+    }
+    for (let i = 0; i < data.groupFields.length; i++) {
+      const groupField = data.groupFields[i];
+      if (groupField.name === '') groupField.name = null;
+      groupField.grade = groupField.grade || null;
+    }
+    setVisible(false);
+    fetchNui('createDoor', data);
+  };
+
   return (
     <Stack justify="space-between" sx={{ height: '100%' }}>
       <Box>
@@ -12,7 +38,7 @@ const General: React.FC = () => {
       </Box>
 
       <Center>
-        <Button color="blue" fullWidth uppercase>
+        <Button color="blue" fullWidth uppercase onClick={() => handleSubmit()}>
           Confirm door
         </Button>
       </Center>
