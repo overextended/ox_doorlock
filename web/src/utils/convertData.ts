a961663634870b37abb56c6c4a05bd64538e82a0
Luke <sabolukas03@gmail.com>
2022-08-12 21:42:18 +0200

    fix(web): insert default groups data if it's not defined

--- web/src/utils/convertData.ts
+++ web/src/utils/convertData.ts
@@ -3,6 +3,7 @@ import { StoreState } from '../store';
 
 // Converts groups data into array format
 export const convertData = (data: DoorColumn) => {
+  if (!data.groups) return { ...data, groups: [{ name: '', grade: 0 }] };
   const doorGroupsData = Object.entries(data.groups);
   let newGroupsData: { name: string; grade: number }[] = [];
   for (let i = 0; i < doorGroupsData.length; i++) {
