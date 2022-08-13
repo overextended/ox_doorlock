bbbc7567e13d7b37b2847e7a3f0a02cb46259619
Linden <65407488+thelindat@users.noreply.github.com>
2022-08-14 06:12:47 +1000

    fix(web): convertData items array

--- web/src/utils/convertData.ts
+++ web/src/utils/convertData.ts
@@ -3,12 +3,13 @@ import { StoreState } from '../store';
 
 // Converts groups data into array format
 export const convertData = (data: DoorColumn) => {
-  if (!data.groups) return { ...data, groups: [{ name: '', grade: 0 }] };
-  const doorGroupsData = Object.entries(data.groups);
   let newGroupsData: { name: string; grade: number }[] = [];
-  for (let i = 0; i < doorGroupsData.length; i++) {
-    const groupObj = doorGroupsData[i];
-    newGroupsData[i] = { name: groupObj[0], grade: groupObj[1] };
+  if (data.groups) {
+    const doorGroupsData = Object.entries(data.groups);
+    for (let i = 0; i < doorGroupsData.length; i++) {
+      const groupObj = doorGroupsData[i];
+      newGroupsData[i] = { name: groupObj[0], grade: groupObj[1] };
+    }
   }
-  return { ...data, groups: [...newGroupsData] } as StoreState;
+  return { ...data, groups: [...newGroupsData], items: data.items || [] } as StoreState;
 };
