a9235fb8b4723cf3401d5667d2b817a3ae11c442
Luke <sabolukas03@gmail.com>
2022-12-11 12:35:16 +0100

    fix(web): remove empty lockpick fields

--- web/src/layouts/settings/views/lockpick/index.tsx
+++ web/src/layouts/settings/views/lockpick/index.tsx
@@ -1,9 +1,16 @@
 import Layout from '../../Layout';
 import { useSetters } from '../../../../store';
 import LockpickFields from './components/LockpickFields';
+import { useEffect } from 'react';
 
 const Lockpick: React.FC = () => {
   const setLockpickFields = useSetters((setter) => setter.setLockpickDifficulty);
+
+  useEffect(() => {
+    return () => {
+      setLockpickFields((prevState) => prevState.filter((field, index) => index === 0 || field !== ''));
+    };
+  }, []);
 
   return (
     <Layout setter={() => setLockpickFields((prevState) => [...prevState, ''])}>
