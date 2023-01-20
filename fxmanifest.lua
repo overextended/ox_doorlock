808c0f2dee6f3e4663fd55e9b72b8f1fbe2b0b55
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2023-01-20 03:28:01 +0000

    chore: bump manifest version to v1.8.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ game         'gta5'
 
 --[[ Resource Information ]]--
 name         'ox_doorlock'
-version      '1.8.2'
+version      '1.8.3'
 license      'GPL-3.0-or-later'
 author       'Overextended'
 repository   'https://github.com/overextended/ox_doorlock'
