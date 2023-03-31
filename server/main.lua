41ea89488015827816b51ef7dc44c40855fbe9e1
Linden <65407488+thelindat@users.noreply.github.com>
2023-03-31 15:02:12 +1100

    Revert "refactor(server): default door authorisation to true"
    This reverts commit 0fad66eb5a6caf095acd83167e2a2dd6a29579ea.

--- server/main.lua
+++ server/main.lua
@@ -204,9 +204,11 @@ local lockpickItems = {
 }
 
 local function isAuthorised(playerId, door, lockpick, passcode)
-	if door.passcode and passcode ~= door.passcode then return false end
+	local player, authorised = GetPlayer(playerId)
 
-	local player, authorised = GetPlayer(playerId), true
+	if passcode and passcode ~= door.passcode then
+		return false
+	end
 
 	if player then
 		if lockpick then
